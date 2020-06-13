77247e61841ac89c9714b48008a99b933b23718f
bokor-git <bogdankorolbk@gmail.com>
2020-06-13 16:27:11 +0300

    create server with html files

--- index.js
+++ index.js
@@ -6,15 +6,64 @@
 //
 // console.log(__filename);
 //})
-
+const fs = require("fs")
+const path = require("path")
 const http = require("http")
 
 //Create server
 const server = http.createServer(((req, res) => {
-    res.writeHead(200, {
-        "Content-type": "text/html"
-    })
-    res.end("<h1>Hello Node JS!</h1>")
+
+    // if(req.url==="/"){
+    //     fs.readFile(path.join(__dirname,"public","index.html"),((err, data) => {
+    //         if(err){
+    //             throw err
+    //         }
+    //         res.writeHead(200, {
+    //             "Content-type": "text/html"
+    //         })
+    //         res.end(data)
+    //     }))
+    // }
+    // else if(req.url==="/contact"){
+    //     fs.readFile(path.join(__dirname,"public","contact.html"),((err, data) => {
+    //         if(err){
+    //             throw err
+    //         }
+    //         res.writeHead(200, {
+    //             "Content-type": "text/html"
+    //         })
+    //         res.end(data)
+    //     }))
+    // }
+
+    // Check URL
+    let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url)
+    const ext = path.extname(filePath)
+    if (!ext) {
+        filePath += ".html"
+    }
+    fs.readFile(filePath, ((err, content) => {
+            if (err) {
+                fs.readFile(path.join(__dirname, "public", "error.html"), ((err, data) => {
+                        if (err) {
+                            res.writeHead(500)
+                            res.end("Error")
+                        } else {
+                            res.writeHead(200, {
+                                "Content-type": "text/html"
+                            })
+                            res.end(data)
+                        }
+                    }
+                ))
+            } else {
+                res.writeHead(200, {
+                    "Content-type": "text/html"
+                })
+                res.end(content)
+            }
+        }
+    ))
 }))
 // Start server
 server.listen(3000, () => {
