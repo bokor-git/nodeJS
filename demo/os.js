b64c3ad2c7d0ebcf2de9d80e8b2bdb8d17f887c4
bokor-git <bogdankorolbk@gmail.com>
2020-06-13 13:30:52 +0300

    os methods

---
+++ demo/os.js
@@ -0,0 +1,18 @@
+
+const os = require("os")
+
+console.log("System:", os.platform())
+
+console.log("Processor arch:", os.arch())
+
+console.log("Processor info:", os.cpus())
+
+console.log("Free memory:", os.freemem())
+
+console.log("Total memory:", os.totalmem())
+
+console.log("Home dir:", os.homedir())
+
+console.log("Uptime:", os.uptime())
+
+
